2e0c4e8f60f3bbf9739ea41edfdfb440f171ae7d
ratushnyyvm <ratushnyyvm@gmail.com>
2022-07-12 21:43:00 +0300

    upd gendiff/scripts/gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.'
+    )
+
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+
+    parser.parse_args()
+
+
+if __name__ == '__main__':
+    main()
