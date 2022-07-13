e7dbfd51b1a6d998aa9fb43e3147cbe97c3255c5
ratushnyyvm <ratushnyyvm@gmail.com>
2022-07-13 18:50:50 +0300

    upd gendiff/scripts/gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -9,6 +9,9 @@ def main():
 
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument(
+        '-f', '--format', metavar='FORMAT', help='set format of output'
+    )
 
     parser.parse_args()
 
