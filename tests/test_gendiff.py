110ef26ed20996ac325e60feec576d7b49365390
ratushnyyvm <ratushnyyvm@gmail.com>
2022-07-21 22:21:05 +0300

    upd tests/test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,5 +1,11 @@
 from gendiff.gendiff import stringify
+from gendiff import generate_diff  # noqa
 
 
 def test_stringify():
     assert stringify(5) == '5'
+
+
+def test_generate_diff():
+    pass
+
