b20dc2fb0515008835a944bbf25caf9cf5473415
ratushnyyvm <ratushnyyvm@gmail.com>
2022-07-24 17:45:05 +0300

    add gendiff/parser.py

---
+++ gendiff/parser.py
@@ -0,0 +1,22 @@
+import argparse
+
+
+def parse():
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.'
+    )
+
+    parser.add_argument('first_file', type=str)
+    parser.add_argument('second_file', type=str)
+
+    parser.add_argument(
+        '-f', '--format',
+        metavar='FORMAT',
+        help='set format of output',
+        choices=['stylish', 'plain', 'json'],
+        default='stylish'
+    )
+
+    args = parser.parse_args()
+
+    return args
