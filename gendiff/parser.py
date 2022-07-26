b2f45967e2ca71409eac90b6801d25283ac1f7c5
ratushnyyvm <ratushnyyvm@gmail.com>
2022-07-26 18:58:49 +0300

    upd gendiff/parser.py

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -1,14 +1,18 @@
 import argparse
 
 
-def parse():
+def parse_input():
     parser = argparse.ArgumentParser(
         description='Compares two configuration files and shows a difference.'
     )
-
-    parser.add_argument('first_file', type=str)
-    parser.add_argument('second_file', type=str)
-
+    parser.add_argument(
+        'first_file',
+        type=str
+    )
+    parser.add_argument(
+        'second_file',
+        type=str
+    )
     parser.add_argument(
         '-f', '--format',
         metavar='FORMAT',
@@ -16,7 +20,6 @@ def parse():
         choices=['stylish', 'plain', 'json'],
         default='stylish'
     )
-
     args = parser.parse_args()
 
     return args
