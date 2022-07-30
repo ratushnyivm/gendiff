5f71034ec80265ee6b2026de01cca14af08e9115
ratushnyyvm <ratushnyyvm@gmail.com>
2022-07-30 16:55:25 +0300

    upd gendiff/gendiff.py

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -6,6 +6,21 @@ from gendiff.output_views.plain import plain_output
 from gendiff.output_views.json import json_output
 
 
+STATUS = 'status'
+ADDED = 'added'
+DELETED = 'deleted'
+CHANGED = 'changed'
+UNCHANGED = 'unchanged'
+
+VALUE = 'value'
+OLD_VALUE = 'old_value'
+NEW_VALUE = 'new_value'
+
+STYLISH = 'stylish'
+PLAIN = 'plain'
+JSON = 'json'
+
+
 def extract_data(path: str) -> dict:
     absolute_path = os.path.abspath(path)
 
@@ -23,50 +38,40 @@ def make_diff(file1: dict, file2: dict) -> dict:
     result = {}
 
     for key in keys:
-        # КЛЮЧ ОТСУТСТВУЕТ В ПЕРВОМ ФАЙЛЕ И ПРИСУТСТВУЕТ ВО ВТОРОМ
         if key not in file1:
             result[key] = {
-                'status': 'added',
-                'value': file2.get(key)
+                STATUS: ADDED,
+                VALUE: file2.get(key)
             }
-        # КЛЮЧ ПРИСУТСТВУЕТ В ПЕРВОМ ФАЙЛЕ И ОТСУТСТВУЕТТ ВО ВТОРОМ
         elif key not in file2:
-
             result[key] = {
-                'status': 'deleted',
-                'value': file1.get(key)
+                STATUS: DELETED,
+                VALUE: file1.get(key)
             }
-
-        # ПАРЫ КЛЮЧ/ЗНАЧЕНИЯ ОДИНАКОВЫ В ОБОИХ ФАЙЛАХ
         elif file1[key] == file2[key]:
-
             if isinstance(file1[key], dict) and \
                     isinstance(file2[key], dict):
-
                 result[key] = {
-                    'status': 'unchanged',
-                    'value': make_diff(file1[key], file2[key])
+                    STATUS: UNCHANGED,
+                    VALUE: make_diff(file1[key], file2[key])
                 }
-
             else:
                 result[key] = {
-                    'status': 'unchanged',
-                    'value': file1.get(key)
+                    STATUS: UNCHANGED,
+                    VALUE: file1.get(key)
                 }
-        # КЛЮЧ ПРИСУТСТВУЕТ В ОБОИХ ФАЙЛАХ, ЗНАЧЕНИЯ ОТЛИЧАЮТСЯ
         else:
-            # ЗНАЧЕНИЯ В ОБОИХ ФАЙЛАХ ЯВЛЯЮТСЯ СЛОВАРЯМИ
             if isinstance(file1[key], dict) and \
                     isinstance(file2[key], dict):
                 result[key] = {
-                    'status': 'changed',
-                    'value': make_diff(file1[key], file2[key])
+                    STATUS: CHANGED,
+                    VALUE: make_diff(file1[key], file2[key])
                 }
             else:
                 result[key] = {
-                    'status': 'changed',
-                    'old_value': file1[key],
-                    'new_value': file2[key]
+                    STATUS: CHANGED,
+                    OLD_VALUE: file1[key],
+                    NEW_VALUE: file2[key]
                 }
 
     return result
@@ -75,19 +80,19 @@ def make_diff(file1: dict, file2: dict) -> dict:
 def generate_diff(
         file_path1: str,
         file_path2: str,
-        format_name: str = 'stylish'
+        format_name: str = STYLISH
 ) -> str:
 
     file_data1, file_data2 = extract_data(file_path1), extract_data(file_path2)
     data_diff = make_diff(file_data1, file_data2)
 
-    if format_name == 'stylish':
+    if format_name == STYLISH:
         return stylish_output(data_diff)
 
-    elif format_name == 'plain':
+    elif format_name == PLAIN:
         return plain_output(data_diff)
 
-    elif format_name == 'json':
+    elif format_name == JSON:
         return json_output(data_diff)
 
     return None
